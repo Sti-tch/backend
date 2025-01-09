2300d17dbb5cd18718b41fea1b6fe9d085cf00a6
kangcheolung <skhu202114111@gmail.com>
2025-01-09 11:30:55 +0900

    add: CampusRepository, MajorRepository, User_Cam_InfoRepository 생성

---
+++ stitch-domain/src/main/java/se/sowl/stitchdomain/user/repository/User_Cam_InfoRepository.java
@@ -0,0 +1,8 @@
+package se.sowl.stitchdomain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.sowl.stitchdomain.user.domain.User_Cam_Info;
+
+public interface User_Cam_InfoRepository extends JpaRepository<User_Cam_Info, Long> {
+
+}
