265b541355e0c8f4d53927557ba830cf6a5ce3c0
kangcheolung <skhu202114111@gmail.com>
2025-02-01 18:08:01 +0900

    bugfix: UserService 수정

--- stitch-api/src/main/java/se/sowl/stitchapi/user/service/UserService.java
+++ stitch-api/src/main/java/se/sowl/stitchapi/user/service/UserService.java
@@ -1,6 +1,9 @@
 package se.sowl.stitchapi.user.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,11 +18,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final CacheManager cacheManager;
 
     public List<User> getList() {
         return userRepository.findAll();
     }
 
+    @Cacheable(value = "user", key = "#userId")
     @Transactional
     public StitchUserInfoResponse getUserInfo(Long userId) {
         User user = userRepository.findById(userId)
@@ -28,6 +33,7 @@ public class UserService {
         return StitchUserInfoResponse.from(user);
     }
 
+    @CacheEvict(value = "user", key = "#userId")
     @Transactional
     public void editUser(Long userId, EditUserRequest request) {
         User user = userRepository.findById(userId).orElseThrow();
