beeb85e2501493cb8a8a66ae7f84d7efd85ce095
kangcheolung <skhu202114111@gmail.com>
2025-05-01 16:40:01 +0900

    bugfix: StudyMemberResponse feat: StudyMemberServiceTest
    - DTO String타입 Enum타입으로 변경
    - StudyMemberServiceTest 생성

--- stitch-api/src/test/java/se/sowl/stitchapi/study/service/StudyMemberServiceTest.java
+++ stitch-api/src/test/java/se/sowl/stitchapi/study/service/StudyMemberServiceTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
+import se.sowl.stitchapi.exception.StudyMemberException;
 import se.sowl.stitchapi.notification.service.NotificationService;
+import se.sowl.stitchapi.study.dto.request.ChangeLeaderRequest;
+import se.sowl.stitchapi.study.dto.request.StudyMemberApplyRequest;
+import se.sowl.stitchapi.study.dto.response.StudyMemberResponse;
 import se.sowl.stitchdomain.school.domain.Campus;
 import se.sowl.stitchdomain.school.repository.CampusRepository;
 import se.sowl.stitchdomain.study.domain.StudyMember;
@@ -21,6 +26,10 @@ import se.sowl.stitchdomain.user.domain.User;
 import se.sowl.stitchdomain.user.domain.UserCamInfo;
 import se.sowl.stitchdomain.user.repository.UserCamInfoRepository;
 import se.sowl.stitchdomain.user.repository.UserRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
@@ -41,10 +50,7 @@ class StudyMemberServiceTest {
     @Autowired
     private UserCamInfoRepository userCamInfoRepository;
 
-    @Autowired
-    private NotificationService notificationService;
-
-    @Autowired
+    @MockBean
     private NotificationService mockNotificationService;
 
     @Autowired
@@ -63,6 +69,7 @@ class StudyMemberServiceTest {
     private StudyPost testStudyPost;
     private StudyMember leaderMember;
     private StudyMember regularMember;
+    private StudyMember pendingMember;
 
 
     @BeforeEach
@@ -153,14 +160,14 @@ class StudyMemberServiceTest {
                 .build();
         regularMember = studyMemberRepository.save(regularMember);
 
-        /* NotificationService mock 설정
-        StudyMemberService)만 테스트하고,
-        그 외 의존하는 서비스(NotificationService)는 모킹하여 격리시킴
-        (즉, 실제 NotificationService의 동작을 수행하지 않음)
-         */
-        doNothing().when(notificationService).createStudyApplyNotification(any(Long.class), any(Long.class));
-        doNothing().when(notificationService).createApproveNotification(any(Long.class));
-        doNothing().when(notificationService).createRejectNotification(any(Long.class));
+        // 대기 중인 신청자 멤버 생성
+        pendingMember = StudyMember.builder()
+                .studyPost(testStudyPost)
+                .userCamInfo(applicantUserCamInfo)
+                .memberRole(MemberRole.APPLICANT)
+                .memberStatus(MemberStatus.PENDING)
+                .build();
+        pendingMember = studyMemberRepository.save(pendingMember);
     }
 
     @Nested
@@ -171,11 +178,185 @@ class StudyMemberServiceTest {
         @DisplayName("스터디 가입 신청 성공")
         void applyStudyMemberSuccess(){
             // given
-
+            StudyMemberApplyRequest request = new StudyMemberApplyRequest(testStudyPost.getId(), "신청 메시지");
 
             //when
+            StudyMemberResponse response = studyMemberService.applyStudyMember(request, applicantUserCamInfo.getId());
 
             // then
+            assertNotNull(response);
+            assertEquals(applicantUserCamInfo.getId(), response.getUserCamInfoId());
+            assertEquals(MemberRole.APPLICANT, response.getMemberRole());
+            assertEquals(MemberStatus.PENDING, response.getMemberStatus());
+
+            // DB에 실제로 반영되었는지 확인
+            Optional<StudyMember>  savedStudyMember = studyMemberRepository.findById(response.getId());
+            assertTrue(savedStudyMember.isPresent());
+            assertEquals(MemberRole.APPLICANT, savedStudyMember.get().getMemberRole());
+            assertEquals(MemberStatus.PENDING, savedStudyMember.get().getMemberStatus());
+        }
+
+        @Test
+        @DisplayName("스터디 가입 신청 시 이미 신청했거나 멤버인 경우 예외 발생")
+        void applyStudyMemberFailAlreadyApplied() {
+            // given
+            StudyMember existingApplication = StudyMember.builder()
+                    .studyPost(testStudyPost)
+                    .userCamInfo(applicantUserCamInfo)
+                    .memberRole(MemberRole.APPLICANT)
+                    .memberStatus(MemberStatus.PENDING)
+                    .build();
+            studyMemberRepository.save(existingApplication);
+
+            StudyMemberApplyRequest request = new StudyMemberApplyRequest(testStudyPost.getId(),"신청 메시지");
+
+            // when & then
+            assertThrows(StudyMemberException.AlreadyAppliedOrMemberException.class,
+                    () -> studyMemberService.applyStudyMember(request, applicantUserCamInfo.getId()));
         }
     }
+
+
+    @Nested
+    @DisplayName("스터디 가입 승인 테스트")
+    class ApproveStudyMemberTest{
+
+        @Test
+        @DisplayName("스터디 가입 승인 성공")
+        void approveStudyMemberSuccess(){
+
+            // when
+            StudyMemberResponse response = studyMemberService.approveStudyMember(pendingMember.getId(), leaderUserCamInfo.getId());
+
+            //then
+            assertNotNull(response);
+            assertEquals(applicantUserCamInfo.getId(), response.getUserCamInfoId());
+            assertEquals(MemberRole.MEMBER, response.getMemberRole());
+            assertEquals(MemberStatus.APPROVED, response.getMemberStatus());
+
+            // DB에 실제로 반영되었는지 확인
+            Optional<StudyMember> savedStudyMember = studyMemberRepository.findById(response.getId());
+            assertTrue(savedStudyMember.isPresent());
+            assertEquals(MemberRole.MEMBER, savedStudyMember.get().getMemberRole());
+            assertEquals(MemberStatus.APPROVED, savedStudyMember.get().getMemberStatus());
+        }
+
+        @Test
+        @DisplayName("스터디 가입 승인 시 리더가 아닌 경우 예외 발생")
+        void approveStudyMemberFailNotLeader(){
+            // when & then
+            assertThrows(StudyMemberException.NotLeaderException.class,
+                    () -> studyMemberService.approveStudyMember(pendingMember.getId(), memberUserCamInfo.getId()));
+        }
+    }
+
+    @Nested
+    @DisplayName("스터디 가입 거절 테스트")
+    class RejectStudyMemberTest{
+
+        @Test
+        @DisplayName("스터디 가입 거절 성공")
+        void rejectStudyMemberSuccess(){
+
+            // when
+            StudyMemberResponse response = studyMemberService.rejectStudyMember(pendingMember.getId(), leaderUserCamInfo.getId());
+
+            //then
+            assertNotNull(response);
+            assertEquals(applicantUserCamInfo.getId(), response.getUserCamInfoId());
+            assertEquals(MemberRole.APPLICANT, response.getMemberRole());
+            assertEquals(MemberStatus.REJECTED, response.getMemberStatus());
+
+            // DB에 실제로 반영되었는지 확인
+            Optional<StudyMember> savedStudyMember = studyMemberRepository.findById(response.getId());
+            assertTrue(savedStudyMember.isPresent());
+            assertEquals(MemberRole.APPLICANT, savedStudyMember.get().getMemberRole());
+            assertEquals(MemberStatus.REJECTED, savedStudyMember.get().getMemberStatus());
+        }
+
+        @Test
+        @DisplayName("스터디 가입 거절 시 리더가 아닌 경우 예외 발생")
+        void rejectStudyMemberFailNotLeader(){
+            // when & then
+            assertThrows(StudyMemberException.NotLeaderException.class,
+                    () -> studyMemberService.rejectStudyMember(pendingMember.getId(), memberUserCamInfo.getId()));
+        }
+    }
+
+    @Nested
+    @DisplayName("스터디 탈퇴 테스트")
+    class LeaveStudyMemberTest{
+
+        @Test
+        @DisplayName("스터디 탈퇴 성공")
+        void leaveStudyMemberSuccess() {
+            // given
+            Long studyPostId = testStudyPost.getId();
+            Long memberUserCamInfoId = memberUserCamInfo.getId();
+
+            // when
+            studyMemberService.leaveStudyMember(studyPostId, memberUserCamInfoId);
+
+            // then
+            // DB에서 삭제되었는지 확인
+            Optional<StudyMember> deletedMember = studyMemberRepository.findByStudyPostAndUserCamInfo(testStudyPost, memberUserCamInfo);
+            assertFalse(deletedMember.isPresent());
+        }
+
+        @Test
+        @DisplayName("스터디 탈퇴 시 스터디 리더인 경우 예외 발생")
+        void leaveStudyMemberFailLeader() {
+            // given
+            Long studyPostId = testStudyPost.getId();
+            Long leaderUserCamInfoId = leaderUserCamInfo.getId();
+
+            // when & then
+            assertThrows(StudyMemberException.LeaderCannotLeaveException.class,
+                    () -> studyMemberService.leaveStudyMember(studyPostId, leaderUserCamInfoId));
+        }
+    }
+
+    @Nested
+    @DisplayName("리더 변경 테스트")
+    class ChangeLeaderTest{
+
+        @Test
+        @DisplayName("스터디 리더 변경 성공")
+        void changeLeaderSuccess(){
+            // given
+            ChangeLeaderRequest request = new ChangeLeaderRequest(testStudyPost.getId(), regularMember.getId());
+
+            //when
+            StudyMemberResponse response = studyMemberService.changeLeader(request, leaderUserCamInfo.getId());
+
+            //then
+            assertNotNull(response);
+            assertEquals(memberUserCamInfo.getId(), response.getUserCamInfoId());
+            assertEquals(MemberRole.LEADER, response.getMemberRole());
+
+            // DB에 실제로 반영되었는지 확인
+            StudyMember updateOldLeader = studyMemberRepository.findById(leaderMember.getId())
+                    .orElseThrow(StudyMemberException.MemberNotFoundException::new);
+            StudyMember updateNewLeader = studyMemberRepository.findById(response.getId())
+                    .orElseThrow(StudyMemberException.MemberNotFoundException::new);
+
+            assertEquals(MemberRole.MEMBER, updateOldLeader.getMemberRole());
+            assertEquals(MemberRole.LEADER, updateNewLeader.getMemberRole());
+        }
+
+        @Test
+        @DisplayName("리더가 아닌 경우 리더 변경 실패")
+        void changeLeaderFailNotLeader() {
+            // given
+            ChangeLeaderRequest request = new ChangeLeaderRequest(testStudyPost.getId(), regularMember.getId());
+
+            // when & then
+            assertThrows(StudyMemberException.NotLeaderException.class,
+                    () -> studyMemberService.changeLeader(request, memberUserCamInfo.getId()));
+        }
+    }
+
 }
+
+
+
